001de4e09ed91bed1862614853da6065d0c53c96
raxl8 <32034076+raxl8@users.noreply.github.com>
2022-07-30 22:11:00 +0200

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,26 @@
+workspace "Guardian"
+    configurations { "Debug", "Release" }
+    platforms "x64"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "obj/%{cfg.buildcfg}/%{prj.name}"
+
+    filter "configurations:Debug"
+        defines "_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "NDEBUG"
+        optimize "On"
+
+include "vendor"
+
+project "Guardian"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+
+    pchheader "pch.h"
+    pchsource "src/pch.cpp"
+
+    includedirs "src" -- fix intellisense not finding pch.h
+    files { "src/**.h", "src/**.cpp" }
