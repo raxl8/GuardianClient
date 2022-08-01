cab106d5dd5307e2576478e5be870e3462ae8c9c
raxl8 <32034076+raxl8@users.noreply.github.com>
2022-08-01 20:09:16 +0200

    chore(vendor): add glfw/glfw@7482de6071d21db77a7236155da44c172a7f6c9e submodule

---
+++ vendor/submodules/glfw.lua
@@ -0,0 +1,31 @@
+return {
+    include = function()
+        includedirs { "include" }
+    end,
+    project = function()
+        kind "StaticLib"
+        language "C"
+
+        files {
+            "src/context.c",
+            "src/egl_context.h",
+            "src/egl_context.c",
+            "src/init.c",
+            "src/input.c",
+            "src/internal.h",
+            "src/mappings.h",
+            "src/monitor.c",
+            "src/osmesa_context.h",
+            "src/osmesa_context.c",
+            "src/vulkan.c",
+            "src/window.c",
+            "src/wgl_context.h",
+            "src/wgl_context.c"
+        }
+
+        if os.istarget('windows') then
+            defines "_GLFW_WIN32"
+            files { "src/win32_*.h", "src/win32_*.c" }
+        end
+    end
+}
