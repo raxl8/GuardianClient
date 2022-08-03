8d4ea2d063899a294a014447dde778e81ea3d7cf
raxl8 <32034076+raxl8@users.noreply.github.com>
2022-08-03 23:08:21 +0200

    feat(Main): platform guarded entry point

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,8 +2,10 @@
 
 #include "Core/Application.h"
 
+#ifdef GDN_WINDOWS
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
 {
-	auto application = std::make_unique<Application>();
+	auto application = MakeUnique<Application>();
 	return application->Run();
 }
+#endif
