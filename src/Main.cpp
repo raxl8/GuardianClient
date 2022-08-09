49c49d7133275a9f5228fafa33e72eaaa9538b30
raxl8 <32034076+raxl8@users.noreply.github.com>
2022-08-09 19:28:57 +0200

    feat(Main): cross-platform entrypoint + add console on non-production builds

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,11 +3,8 @@
 #include "Core/Application.h"
 #include "Hooking/HookFunction.h"
 
-#ifdef GDN_WINDOWS
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
+int RealMain(int argc, char* argv[])
 {
-	HookFunction::RunAll();
-
 	auto application = new Application;
 	Instance<Application>::Set(application);
 
@@ -16,5 +13,33 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	delete application;
 
 	return exitCode;
+}
+
+#ifdef GDN_WINDOWS
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
+{
+	HookFunction::RunAll();
+
+#ifndef GDN_PRODUCTION
+	AllocConsole();
+
+	FILE* dummyStream;
+	freopen_s(&dummyStream, "CONIN$", "r", stdin);
+	freopen_s(&dummyStream, "CONOUT$", "w", stdout);
+	freopen_s(&dummyStream, "CONOUT$", "w", stderr);
+#endif
+
+	auto exitCode = RealMain(__argc, __argv);
+
+#ifndef GDN_PRODUCTION
+	FreeConsole();
+#endif
+
+	return exitCode;
+}
+#else
+int main(int argc, char* argv[])
+{
+	return RealMain(argc, argv);
 }
 #endif
