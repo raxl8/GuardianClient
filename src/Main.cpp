f74282909ec66e77d3c04b50e6faa1818c8fae1c
raxl8 <32034076+raxl8@users.noreply.github.com>
2022-08-10 18:16:22 +0200

    fix(Main): don't run HookFunctions only on Windows

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,6 +5,8 @@
 
 int RealMain(int argc, char* argv[])
 {
+	HookFunction::RunAll();
+
 	auto application = new Application;
 	Instance<Application>::Set(application);
 
@@ -18,8 +20,6 @@ int RealMain(int argc, char* argv[])
 #ifdef GDN_WINDOWS
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
 {
-	HookFunction::RunAll();
-
 #ifndef GDN_PRODUCTION
 	AllocConsole();
 
