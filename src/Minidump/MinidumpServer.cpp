bd95945cd6ed31e9b32f936984939a4de3f155c9
raxl8 <32034076+raxl8@users.noreply.github.com>
2022-08-20 21:36:24 +0200

    feat(MinidumpServer): add message box on crash with crash id

--- src/Minidump/MinidumpServer.cpp
+++ src/Minidump/MinidumpServer.cpp
@@ -2,7 +2,56 @@
 
 #include "MinidumpServer.h"
 
+#include <fmt/printf.h>
 #include <handler/handler_main.h>
+#include <handler/crash_report_upload_thread.h>
+
+void OnUploadedReport(std::string& id)
+{
+	int result = MessageBoxA(NULL, fmt::sprintf(
+		"Whoopsie! " PRODUCT_NAME R"( has crashed, a crash report has been uploaded so we can look further to prevent this error from happening.
+
+Crash ID: %s
+
+Would you like to copy this ID for further assistance?)",
+		id).c_str(), PRODUCT_NAME, MB_TOPMOST | MB_ICONERROR | MB_OK | MB_YESNO);
+
+	switch (result)
+	{
+	case IDYES:
+	{
+		if (OpenClipboard(NULL))
+		{
+			EmptyClipboard();
+			auto data = GlobalAlloc(GMEM_MOVEABLE, id.size() + 1);
+			if (!data)
+			{
+				CloseClipboard();
+				break;
+			}
+
+			auto lockedData = GlobalLock(data);
+			if (!lockedData)
+			{
+				GlobalFree(data);
+				CloseClipboard();
+				break;
+			}
+
+			memcpy(lockedData, id.c_str(), id.size() + 1);
+			GlobalUnlock(data);
+
+			SetClipboardData(CF_TEXT, data);
+			CloseClipboard();
+			GlobalFree(data);
+		}
+		break;
+	}
+	case IDNO:
+	default:
+		break;
+	}
+}
 
 int StartMinidumpServer(int argc, char* argv[])
 {
@@ -19,6 +68,8 @@ int StartMinidumpServer(int argc, char* argv[])
 	}
 
 	crashpadArgv[argc] = nullptr;
+
+	crashpad::CrashReportUploadThread::SetOnUploadedReportHandler(OnUploadedReport);
 
 	return crashpad::HandlerMain(crashpadArgc, crashpadArgv.get(), nullptr);
 }
