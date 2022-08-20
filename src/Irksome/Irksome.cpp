bfaded22e3ea796f677fa1571af9961e2017cb8f
raxl8 <32034076+raxl8@users.noreply.github.com>
2022-08-20 13:37:00 +0200

    fix(Irksome): detach heartbeat thread OnDisconnect will be called from it when its SendPacket tries to send to a closed connection, leading to thread finishing its task but not being joined

--- src/Irksome/Irksome.cpp
+++ src/Irksome/Irksome.cpp
@@ -4,38 +4,32 @@
 #include "Networking/PacketID.h"
 #include "Networking/Websocket.h"
 
-static std::mutex g_HeartbeatMutex;
-static std::condition_variable g_HeartbeatConditionVariable;
-static std::thread g_HeartbeatThread;
-
 static HookFunction hookFunction([]()
 {
 	Websocket::OnInitializeInstance.Subscribe([](Websocket* instance)
 	{
 		instance->OnHandshaked.Subscribe([instance]()
 		{
-			g_HeartbeatThread = std::thread([instance]()
+			static std::mutex conditionMutex;
+			static std::condition_variable conditionVariable;
+
+			std::thread([instance]()
 			{
 				while (instance->IsConnected())
 				{
 					PacketStream packet;
 					packet.Write<PacketID>(PacketID::Heartbeat);
-					instance->SendPacket(packet);
+					if (!instance->SendPacket(packet))
+						continue;
 
-					std::unique_lock lock(g_HeartbeatMutex);
-					g_HeartbeatConditionVariable.wait_for(lock, std::chrono::seconds(30));
+					std::unique_lock lock(conditionMutex);
+					conditionVariable.wait_for(lock, std::chrono::seconds(5));
 				}
-			});
+			}).detach();
 
 			instance->OnDisconnect.Subscribe([]()
 			{
-				if (g_HeartbeatThread.joinable())
-				{
-					g_HeartbeatConditionVariable.notify_one();
-					// Don't try to join on current thread, would cause deadlock
-					if (g_HeartbeatThread.get_id() != std::this_thread::get_id())
-						g_HeartbeatThread.join();
-				}
+				conditionVariable.notify_one();
 			});
 		});
 	});
